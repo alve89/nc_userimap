f09818bbf428059e799d8d99215364da580c0c72
alve89 <info@stefan-herzog.com>
2019-08-26 17:28:25 +0200

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,4 +3,4 @@
 //$app = new \OCA\UserIMAP\AppInfo\Application();
 //$app->register();
 
-OC::$CLASSPATH['OC_User_IMAP']='userimap/lib/imap.php';
+OC::$CLASSPATH['OC_User_IMAP_w_UD']='userimap/lib/imap.php';
